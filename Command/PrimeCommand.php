13d3acf8649765b4d06af927db15e4adde412384
lukaszwitczak <lukasz.witczak@lemondemon.pl>
2017-02-22 21:58:16 +0100

    initial commit with working command and basic tests

---
+++ Command/PrimeCommand.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lukaszwitczak
+ * Date: 20/02/17
+ * Time: 21:29
+ */
+class PrimeCommand
+{
+
+}
---
+++ Tests/Command/PrimeCommandTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lukaszwitczak
+ * Date: 20/02/17
+ * Time: 21:46
+ */
+class PrimeCommandTest
+{
+
+}
