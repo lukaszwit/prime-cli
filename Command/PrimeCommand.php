6af765663f21809b9e6a2e94755dcea9f7eb9cf9
lukaszwitczak <lukasz.witczak@lemondemon.pl>
2017-02-22 22:02:44 +0100

    Initial commit with working command and basic tests

--- Command/PrimeCommand.php
+++ Command/PrimeCommand.php
@@ -1,12 +1,185 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: lukaszwitczak
- * Date: 20/02/17
- * Time: 21:29
- */
-class PrimeCommand
-{
+namespace Lukaszwit\Prime\Cli\Command;
 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Component\Stopwatch\StopwatchEvent;
+
+class PrimeCommand extends Command
+{
+    private $stopwatch;
+
+    private $howManyPrimesToFind;
+
+    private $showSummary = true;
+
+    private $showPrimes = false;
+
+    private $optimize;
+
+    protected function configure()
+    {
+        $this
+            // the name of the command (the part after "bin/console")
+            ->setName('prime')
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Prime computer.')
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp("This command allows you to ctest what command can do")
+            // configure an argument
+            ->addArgument('howManyPrimesToFind', InputArgument::OPTIONAL, 'I need your name to proceed.')
+            ->addOption('summary', 's', InputOption::VALUE_NONE, 'Do you want to see summary after command finished?')
+            ->addOption(
+                'showPrimes',
+                'p',
+                InputOption::VALUE_NONE,
+                'Do you want to see all prime numbers after command finished?'
+            )
+            ->addOption(
+                'force',
+                'f',
+                InputOption::VALUE_NONE,
+                'Do you want to continue even if operation might me time consuming?'
+            )
+            ->addOption('optimize', 'o', InputOption::VALUE_OPTIONAL, 'Do you want to use square optimization? [y/n]', 'y');
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $questionHelper = $this->getHelper('question');
+
+        $this->howManyPrimesToFind = $input->getArgument('howManyPrimesToFind');
+        if (!$this->howManyPrimesToFind) {
+            $question = new Question('Please enter the number of primes you wish to find: ', 2);
+            $this->howManyPrimesToFind = $questionHelper->ask($input, $output, $question);
+        }
+
+        if ($this->howManyPrimesToFind < 2) {
+            $formatter = $this->getHelper('formatter');
+            $errorMessages = array('Error!', 'You need to provide positive number greater than 1');
+            $formattedBlock = $formatter->formatBlock($errorMessages, 'error', true);
+            $output->writeln($formattedBlock);
+
+            exit(1);
+        }
+
+        $force = $input->getOption('force');
+        if (!$force && $this->howManyPrimesToFind > 1000) {
+            $question = new ConfirmationQuestion('This operation might take a while. Do you wish to continue? ', false);
+            if (!$questionHelper->ask($input, $output, $question)) {
+                exit(0);
+            }
+        }
+
+        $this->showSummary = $input->getOption('summary');
+        $this->optimize = $input->getOption('optimize');
+        $this->showPrimes = $input->getOption('showPrimes');
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $formatter = $this->getHelper('formatter');
+
+        $formattedBlock = $formatter->formatSection('Welcome', 'You are running super slow prime numbers finder', 'info');
+        $output->writeln($formattedBlock);
+
+        $this->stopwatch = new Stopwatch();
+        $this->stopwatch->start('prime');
+        $foundPrimes = $this->findNprimes($this->howManyPrimesToFind, $output);
+        $output->writeln('');
+        $event = $this->stopwatch->stop('prime');
+
+        if ($this->showPrimes) {
+            $this->showPrimes($foundPrimes, $output);
+        }
+
+        if ($this->showSummary) {
+            $this->showSummary($foundPrimes, $event, $output);
+        }
+
+//        $errorMessages = array('Error!', 'Something went wrong');
+//        $formattedBlock = $formatter->formatBlock($errorMessages, 'error', true);
+//        $output->writeln($formattedBlock);
+
+        $output->writeln('All operation are done. This is the end!');
+    }
+
+    private function showPrimes(array $foundPrimes, OutputInterface $output)
+    {
+        $formatter = $this->getHelper('formatter');
+        $formattedLine = $formatter->formatSection(
+            'Found primes',
+            implode(', ', $foundPrimes)
+        );
+
+        $output->writeln($formattedLine);
+    }
+
+    private function showSummary(array $foundPrimes, StopwatchEvent $event, OutputInterface $output)
+    {
+        $table = new Table($output);
+        $table
+            ->setHeaders(['Prime numbers found', 'Total computing time']);
+        $table->addRow([count($foundPrimes), $event->getDuration() . ' ms']);
+
+        $table->render();
+    }
+
+    private function findNprimes($howManyPrimesToFind, $output)
+    {
+        $progress = new ProgressBar($output, $howManyPrimesToFind);
+        $progress->start();
+
+        $startNumber = 2;
+        $currentNumber = $startNumber;
+        $foundNumbersCount = 0;
+        $foundNumbers = [];
+        $howManyPrimesToFind = (int)$howManyPrimesToFind;
+
+        while ($foundNumbersCount < $howManyPrimesToFind) {
+            if ($this->isPrime($currentNumber)) {
+                ++$foundNumbersCount;
+                $foundNumbers[] = $currentNumber;
+                $progress->advance();
+                $this->stopwatch->lap('prime');
+            }
+
+            ++$currentNumber;
+        }
+
+        $progress->finish();
+
+        return $foundNumbers;
+    }
+
+    private function isPrime($numberToCheck)
+    {
+        $iterationsCount = $numberToCheck;
+        if ('y' === $this->optimize) {
+            $iterationsCount = sqrt($numberToCheck);
+        }
+
+        if ($numberToCheck < 2) {
+            return false;
+        } else {
+            for ($i = 2; $i < $iterationsCount; $i++) {
+                if (($numberToCheck % $i) === 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
--- Tests/Command/PrimeCommandTest.php
+++ Tests/Command/PrimeCommandTest.php
@@ -1,12 +1,35 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: lukaszwitczak
- * Date: 20/02/17
- * Time: 21:46
- */
-class PrimeCommandTest
-{
+namespace Tests\Lukaszwit\Prime\Cli\Command;
 
+use Lukaszwit\Prime\Cli\Command\PrimeCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class PrimeCommandTest extends KernelTestCase
+{
+    public function testPrime()
+    {
+        self::bootKernel();
+        $application = new Application(self::$kernel);
+
+        $application->add(new PrimeCommand());
+
+        $command = $application->find('prime');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'  => $command->getName(),
+            'howManyPrimesToFind' => '11',
+
+            // prefix the key with a double slash when passing options,
+            // e.g: '--some-option' => 'option_value',
+        ));
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertContains('You are running super slow prime numbers finder', $output);
+
+        // ...
+    }
 }
